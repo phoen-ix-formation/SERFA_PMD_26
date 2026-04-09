1701e72a6226ed1390bfe8ed17e3498dd2a363eb
karhel <karheltmarr@hotmail.fr>
2026-04-09 15:28:48 +0200

    06-Loading + JS

--- animations/06-loader/script.js
+++ animations/06-loader/script.js
@@ -1,1 +1,21 @@
-// On modifie la vitesse de l'animation en fonction de la valeur de l'input
+// On attend le chargement du DOM
+document.addEventListener('DOMContentLoaded', e => {
+
+    const arrElDots = document.getElementsByClassName('dot');
+
+    // Ecoute l'évènement input sur l'élément du DOM ayant l'id = speed
+    document.getElementById('speed')
+        .addEventListener('input', ev => {
+
+        // Récupère la valeur de l'input dans une variable
+        const flDuration = ev.target.value;
+
+        // Boucle pour chaque div .dot de mon tableau
+        for(const elDot of arrElDots) {
+
+            // Modifie la propriété CSS animation-duration
+            // avec la valeur flDuration 
+            elDot.style.animationDuration = `${flDuration}s`;
+        }
+    });
+});
