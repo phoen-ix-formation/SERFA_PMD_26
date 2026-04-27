03370201dea160433ac94a2d2f593b9f8179434a
karhel <karheltmarr@hotmail.fr>
2026-04-27 13:15:52 +0200

    Première animation

--- canvas/03-animations/script.js
+++ canvas/03-animations/script.js
@@ -1,1 +1,24 @@
 // Script partie 3 - Animations
+const elCanvas = document.getElementById('drawing');
+const ctx = elCanvas.getContext('2d');
+
+// Poisition en X du carré
+let intX = 0;
+
+ctx.fillStyle = 'black';
+
+function animate() {
+
+    // Reset du canvas
+    ctx.clearRect(0, 0, elCanvas.width, elCanvas.height);
+
+    // Dessin du rectangle
+    ctx.fillRect(intX, 100, 30, 30);
+
+    intX++; //< Incrément de la position en X
+
+    // Appeler la frame suivante
+    requestAnimationFrame(animate);
+}
+
+animate(); // Lancement initial
