87a3583e94cc7d6891b574e1380c5c6b110c0f21
karhel <karheltmarr@hotmail.fr>
2026-04-27 13:33:39 +0200

    Rebond carré

--- canvas/03-animations/script.js
+++ canvas/03-animations/script.js
@@ -2,20 +2,35 @@
 const elCanvas = document.getElementById('drawing');
 const ctx = elCanvas.getContext('2d');
 
-// Poisition en X du carré
+// Position en X du carré
 let intX = 0;
+
+// Flag qui indique le sens de déplacement
+let blDirection = true; //< Vrai = G=>D, Faux = D=>G
 
 ctx.fillStyle = 'black';
 
 function animate() {
-
     // Reset du canvas
     ctx.clearRect(0, 0, elCanvas.width, elCanvas.height);
 
     // Dessin du rectangle
     ctx.fillRect(intX, 100, 30, 30);
 
-    intX++; //< Incrément de la position en X
+    // On teste la direction
+    if(blDirection) {
+        intX += 4; //< Incrément de la position en X
+    }
+    else {
+        intX -= 4; //< Décrémente
+    }
+    
+    if(intX + 30 >= elCanvas.width) {
+        blDirection = false;
+    }
+    else if(intX <= 0) {
+        blDirection = true;
+    }
 
     // Appeler la frame suivante
     requestAnimationFrame(animate);
