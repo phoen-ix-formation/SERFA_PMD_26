2f8469411ef15a2b1cd0cf3f5810c6c4167c5b11
karhel <karheltmarr@hotmail.fr>
2026-04-27 14:17:02 +0200

    Gestion de l'état de dessin

--- canvas/04-dessins/script.js
+++ canvas/04-dessins/script.js
@@ -5,46 +5,58 @@ const ctx = elCanvas.getContext('2d');
 let intLastMouseX = 0;
 let intLastMouseY = 0;
 
+let blIsDrawing = false; //< Stocke l'état : en dessin ou non
+
 // Ecoute des évènements de la souris
 // La souris se déplace dans le canvas
 elCanvas.addEventListener('mousemove', (e) => {
-    // On calcule la position relative de la souris dans le canvas
 
-    // 1. on récupère la position du canvas à l'écran
-    const posCanvas = elCanvas.getBoundingClientRect();
+    if(blIsDrawing) {
+        // On calcule la position relative de la souris dans le canvas
 
-    // 2. On soustrait la position du canvas à la position de la souris dans l'écran
-    const intMouseX = e.clientX - posCanvas.left;
-    const intMouseY = e.clientY - posCanvas.top;
+        // 1. on récupère la position du canvas à l'écran
+        const posCanvas = elCanvas.getBoundingClientRect();
 
-    ctx.beginPath();
-    ctx.strokeStyle = "black";
+        // 2. On soustrait la position du canvas à la position de la souris dans l'écran
+        const intMouseX = e.clientX - posCanvas.left;
+        const intMouseY = e.clientY - posCanvas.top;
 
-    ctx.moveTo(intLastMouseX, intLastMouseY);
-    ctx.lineTo(intMouseX, intMouseY);
-    
-    ctx.stroke();
+        ctx.beginPath();
+        ctx.strokeStyle = "black";
 
-    intLastMouseX = intMouseX;
-    intLastMouseY = intMouseY;
+        ctx.moveTo(intLastMouseX, intLastMouseY);
+        ctx.lineTo(intMouseX, intMouseY);
+        
+        ctx.stroke();
+
+        intLastMouseX = intMouseX;
+        intLastMouseY = intMouseY;
+    }
 });
 
 // La souris sort du canvas - sort de l'état dessin
 elCanvas.addEventListener('mouseleave', (e) => {
     
-    
+    blIsDrawing = false;
 });
 
 // On presse le clic-gauche - état de dessin
 elCanvas.addEventListener('mousedown', (e) => {
-    
-    
+    const posCanvas = elCanvas.getBoundingClientRect();
+
+    // On défini l'ancienne position de la souris à la position du clic
+    // Evite que le tracé ne commence là où il s'est arrete juste avant
+    // mais débute bien à l'emplacement du clic
+    intLastMouseX = e.clientX - posCanvas.left;
+    intLastMouseY = e.clientY - posCanvas.top;
+
+    blIsDrawing = true;
 });
 
 // On lache le clic-gauche - sort de l'état dessin
 elCanvas.addEventListener('mouseup', (e) => {
     
-    
+    blIsDrawing = false;
 });
 
 // Lors du clic sur le bouton reset
