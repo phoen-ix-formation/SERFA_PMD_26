f116356954be1a0e9a412c727bd5f4c6990a4656
karhel <karheltmarr@hotmail.fr>
2026-04-27 10:49:48 +0200

    Damier complet à optimiser

--- canvas/01-intro/script.js
+++ canvas/01-intro/script.js
@@ -23,26 +23,41 @@ ctx.fillRect(100, 200, 200, 100);
 
 // Première case, en haut à gauche
 
-ctx.fillStyle = 'black';
-for(let i = 0; i <= intCanvasWidth; i += 100) {
-    ctx.fillRect(i, 0, 50, 50);
+// j => l'incrément en Y
+for(let j = 0; j <= intCanvasHeight; j++) {
+
+    // lignes paires => première case noire
+    // lignes impaires => en blanc
+    // nombre % 2 == 0 => pair
+    // nombre % 2 != 0 => impair
+
+    if(j % 2 == 0) {
+        // Ligne paires
+
+        ctx.fillStyle = 'black';
+        // i => l'incrément en X
+        for(let i = 0; i <= intCanvasWidth; i += 100) {
+            ctx.fillRect(i, j * 50, 50, 50);
+        }
+
+        ctx.fillStyle = 'white';
+        for(let i = 50; i <= intCanvasWidth; i += 100) {
+            ctx.fillRect(i, j * 50, 50, 50);
+        }
+    }
+    else {
+        // Ligne impaire
+
+        ctx.fillStyle = 'white';
+        // i => l'incrément en X
+        for(let i = 0; i <= intCanvasWidth; i += 100) {
+            ctx.fillRect(i, j * 50, 50, 50);
+        }
+
+        ctx.fillStyle = 'black';
+        for(let i = 50; i <= intCanvasWidth; i += 100) {
+            ctx.fillRect(i, j * 50, 50, 50);
+        }
+    }
+
 }
-
-ctx.fillStyle = 'white';
-for(let i = 50; i <= intCanvasWidth; i += 100) {
-    ctx.fillRect(i, 0, 50, 50);
-}
-
-
-/*
-ctx.fillRect(0, 0, 50, 50);
-
-ctx.fillStyle = 'white';
-ctx.fillRect(50, 0, 50, 50);
-
-ctx.fillStyle = 'black';
-ctx.fillRect(100, 0, 50, 50);
-
-ctx.fillStyle = 'white';
-ctx.fillRect(150, 0, 50, 50);
-*/
