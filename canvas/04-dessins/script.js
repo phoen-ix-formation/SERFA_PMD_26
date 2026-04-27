62ea18f150aa400344710b064b4ec15c435d85cb
karhel <karheltmarr@hotmail.fr>
2026-04-27 14:08:52 +0200

    Dessin en continu

--- canvas/04-dessins/script.js
+++ canvas/04-dessins/script.js
@@ -2,10 +2,31 @@
 const elCanvas = document.getElementById('canvas');
 const ctx = elCanvas.getContext('2d');
 
+let intLastMouseX = 0;
+let intLastMouseY = 0;
+
 // Ecoute des évènements de la souris
 // La souris se déplace dans le canvas
 elCanvas.addEventListener('mousemove', (e) => {
-    console.log(e);
+    // On calcule la position relative de la souris dans le canvas
+
+    // 1. on récupère la position du canvas à l'écran
+    const posCanvas = elCanvas.getBoundingClientRect();
+
+    // 2. On soustrait la position du canvas à la position de la souris dans l'écran
+    const intMouseX = e.clientX - posCanvas.left;
+    const intMouseY = e.clientY - posCanvas.top;
+
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+
+    ctx.moveTo(intLastMouseX, intLastMouseY);
+    ctx.lineTo(intMouseX, intMouseY);
+    
+    ctx.stroke();
+
+    intLastMouseX = intMouseX;
+    intLastMouseY = intMouseY;
 });
 
 // La souris sort du canvas - sort de l'état dessin
