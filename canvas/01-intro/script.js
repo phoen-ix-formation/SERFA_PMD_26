0151fdb4c03d2464dc4a0876f9c4df63dd65527a
karhel <karheltmarr@hotmail.fr>
2026-04-27 09:38:22 +0200

    Créer des rectangles

--- canvas/01-intro/script.js
+++ canvas/01-intro/script.js
@@ -1,1 +1,19 @@
 // Script Partie 1 - Introduction
+const elCanvas = document.getElementById('drawing');
+const ctx = elCanvas.getContext('2d');
+
+// La taille du canvas
+const intCanvasWidth    = elCanvas.width;
+const intCanvasHeight   = elCanvas.height;
+
+
+ctx.fillStyle = 'blue';
+ctx.fillRect(100, 100, 200, 100);
+
+// Deuxième rectangle de même taille en rouge, 
+// colé à droite de l'autre
+ctx.fillStyle = 'red';
+ctx.fillRect(300, 100, 200, 100);
+
+ctx.fillRect(100, 200, 200, 100);
+
